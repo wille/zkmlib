633f7efd228285cfaac674927dd866bde09c1787
redpois0n <git@redpois0n>
2014-01-02 21:36:42 +0100

    Args Utils

--- src/com/redpois0n/zkmlib/Main.java
+++ src/com/redpois0n/zkmlib/Main.java
@@ -2,8 +2,32 @@ package com.redpois0n.zkmlib;
 
 public class Main {
 	
+	/**
+	 * Main entry point
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
+	}
+	
+	public static String getArg(String arg, String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equalsIgnoreCase(arg)) {
+				return args[i + 1];
+			}
+		}
+		
+		return null;
+	}
+	
+	public static boolean argsContains(String arg, String[] args) {
+		for (String s : args) {
+			if (arg.equalsIgnoreCase(s)) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 
 }
