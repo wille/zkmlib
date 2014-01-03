208dea4ac277a3eb7ee2e8d86873fc4ef532ac78
redpois0n <git@redpois0n>
2014-01-03 22:02:30 +0100

    Add UpdateListeners

--- src/com/redpois0n/zkmlib/Obfuscator.java
+++ src/com/redpois0n/zkmlib/Obfuscator.java
@@ -2,41 +2,50 @@ package com.redpois0n.zkmlib;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Obfuscator {
-	
+
 	private File input;
 	private File output;
 	private String mainClass;
-	
+	private List<UpdateListener> listeners = new ArrayList<UpdateListener>();
+
+
 	public Obfuscator(File input, File output, String mainClass) {
 		this.input = input;
 		this.output = output;
 		this.mainClass = mainClass;
 	}
-	
-	public void obfuscate() throws Exception {		
+
+	public void obfuscate() throws Exception {
 		ScriptGenerator sg = new ScriptGenerator(input, output, mainClass);
-		
+
 		String script = sg.generate();
-		
+
 		File scriptFile = File.createTempFile("zkmlib", ".txt");
 		FileOutputStream fos = new FileOutputStream(scriptFile);
 		fos.write(script.getBytes("UTF-8"));
 		fos.close();
-		
+
 		JavaProcess jp = new JavaProcess(Runtime.getRuntime().exec(new String[] { Main.JAVA_HOME, "-jar", Main.getZKMJar().getAbsolutePath(), scriptFile.getAbsolutePath() }));
-		
-		jp.addListener(new UpdateListener() {
-			@Override
-			public void onInput(String s) {
-				
-			}			
-		});
-	
+
+		for (UpdateListener listener : listeners) {
+			jp.addListener(listener);
+		}
+
 		jp.waitFor();
-		
+
 		scriptFile.delete();
-	
+
+	}
+
+	public void addListener(UpdateListener listener) {
+		listeners.add(listener);
+	}
+
+	public void removeListener(UpdateListener listener) {
+		listeners.remove(listener);
 	}
 }
